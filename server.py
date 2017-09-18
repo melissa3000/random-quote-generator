b9436247c361a813d91a36b2a505867f9d4bf902
Melissa Boyer <melissaboyer3000@gmail.com>
2017-09-18 13:50:19 -0400

    Adding Random Word Generator before deployment

---
+++ server.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, render_template, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+import unirest
+
+
+
+app = Flask(__name__)
+
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+
+
+@app.route("/")
+def index():
+    """Return home page."""
+
+
+    return render_template("index.html")
+
+@app.route("/quote")
+def get_random_quote():
+
+    """Return a random quote."""
+
+
+    response = unirest.get("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1",
+                         headers={"X-Mashape-Key": "kG28Ro8HNgmshL7xJDQY6caHHHiLp1lZpHijsndDiJ3zb3fQH0",
+                                  "Accept": "application/json"})
+
+    # quote = response.body['quote']
+    # author = response.body['author']
+    # print quote, author
+
+
+    return jsonify(response.body)
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+
+    app.run(host="0.0.0.0")
